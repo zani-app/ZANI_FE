05ad5ccc92894fdd8ef1151de40a8524e3210ade
JungDohyeon <40754281+JungDohyeon@users.noreply.github.com>
2024-03-14 21:05:25 +0900

    Initial Commit

---
+++ ZANI/ZANIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZANIApp.swift
+//  ZANI
+//
+//  Created by 정도현 on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ZANIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
