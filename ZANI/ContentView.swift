d46e87e8e6581f868299c24b9bc3123d08c82e1b
JungDohyeon <40754281+JungDohyeon@users.noreply.github.com>
2024-03-15 21:54:08 +0900

    refactor: contentView Vstack 제거

--- ZANI/ContentView.swift
+++ ZANI/ContentView.swift
@@ -9,28 +9,25 @@ import SwiftUI
 
 public struct ContentView: View {
   public var body: some View {
-    VStack {
-      TabView(
-        selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/,
+    TabView(
+      selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/,
+      
+      content: {
         
-        content: {
-          
-          MateMain()
-            .tabItem { Text("밤샘메이트") }.tag(1)
-          
-          RecruitmentMain()
-            .tabItem { Text("모집페이지") }.tag(1)
-          
-          CommunityMain()
-            .tabItem { Text("커뮤니티") }.tag(1)
-          
-          MyPageMain()
-            .tabItem { Text("마이페이지") }.tag(1)
-          
-        }
-      )
-    }
-    .padding()
+        MateMain()
+          .tabItem { Text("밤샘메이트") }.tag(1)
+        
+        RecruitmentMain()
+          .tabItem { Text("모집페이지") }.tag(1)
+        
+        CommunityMain()
+          .tabItem { Text("커뮤니티") }.tag(1)
+        
+        MyPageMain()
+          .tabItem { Text("마이페이지") }.tag(1)
+        
+      }
+    )
   }
 }
 
